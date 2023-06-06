b5be4688facc79cc20fb2c0e1133906e359ed52d
Anekant <Anekantjainsagar@gmail.com>
2023-06-06 16:03:42 +0530

    res to response in delete

--- Routes/educators.js
+++ Routes/educators.js
@@ -106,8 +106,8 @@ educators.post("/addEducator", async (req, res) => {
 
 educators.delete("/deleteEducator", (req, res) => {
   const { id } = req.body;
-  Educator.deleteOne({ _id: id }).then((res) => {
-    res.send(res);
+  Educator.deleteOne({ _id: id }).then((response) => {
+    res.send(response);
   });
 });
 
